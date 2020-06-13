56443700e36df5a2af30854ef45f6179ecb87aab
dishonestea <vishay8@gmail.com>
2020-06-13 15:12:14 -0500

    added sound

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ting from './ting.wav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: 300,
+      isActive: false,
+      isEnded: false
+    };
+  }
+
+    secondUp = () => {
+      this.setState((state, props) => ({
+        timer: state.timer + 1
+      }));
+    }
+
+    secondDown = () => {
+      this.setState((state, props) => ({
+        timer: state.timer - 1
+      }));
+    }
+
+    minuteUp = () => {
+      this.setState((state, props) => ({
+        timer: state.timer + 60
+      }));
+    }
+
+    minuteDown = () => {
+      this.setState((state, props) => ({
+        timer: state.timer - 60
+      }));
+    }
+
+    pauseClicked = () => {
+      if (this.state.isActive === true && this.state.isEnded === false) {
+        this.timerID = setInterval(
+          () => this.secondDown(),
+          1000);}
+          else {
+            clearInterval(this.timerID);
+          }
+      }
+
+    resetClicked = () => {
+    this.setState({
+      timer: 300,
+      isActive: false,
+      isEnded: false
+      });
+      clearInterval(this.timerID);
+    }
+    
+    pauseButton = () => {
+      if(this.state.isEnded === false){
+      this.setState(prevState => ({
+        isActive: !prevState.isActive
+      })
+      , () => this.pauseClicked(),)
+    }
+    }
+
+    showTime = () => {
+      let minutes = Math.floor(this.state.timer/60);
+      let seconds = this.state.timer%60;
+      
+      if(seconds > 9){
+      return(
+      <h1 className = 'largerFont'>{this.state.isEnded? "Time's Up" :
+      `${minutes}:${seconds}`
+    }</h1>);
+  } else {
+    return(
+      <h1 className = 'largerFont'>{this.state.isEnded? "Time's Up" :
+      `${minutes}:0${seconds}`
+    }</h1>);
+  }
+  
+
+    }
+    
+    componentDidUpdate() {
+      if (this.state.timer <= 0 && this.state.isEnded === false) {
+        this.play();
+        clearInterval(this.timerID);
+        this.setState({
+          isActive: false,
+          isEnded: true
+        })
+      }
+    }
+     audio = new Audio(ting);
+
+    play = () => {
+      this.audio.play();
+    }
+
+  render() {
+
+    return (
+      <div className = 'App'>
+
+        <h2 className = 'title'>Clock</h2>
+        {this.showTime()}
+        <div className = 'times'>
+        <p>Minutes</p>
+        <div className = 'minutes'>
+        <button className = 'timeButton' onClick={() => this.minuteUp()}>+</button>
+        <button className = 'timeButton' onClick={() => this.minuteDown()}>-</button>
+        </div>
+        <p>Seconds</p>
+        <div className = 'seconds'>
+        <button className = 'timeButton' onClick={() => this.secondUp()}>+</button>
+        <button className = 'timeButton' onClick={() => this.secondDown()}>-</button>
+        </div>
+        </div>
+        <div className = 'resume'>
+        <button className = 'resumeButton' onClick={() => this.pauseButton()}>{this.state.isActive ? "Pause" : "Start"}</button>
+        </div>
+        <div className = 'reset'>
+        <button className = 'resetButton' onClick={() => this.resetClicked()}>Reset</button>
+        </div>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
